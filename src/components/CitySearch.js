89e4d385169c70848a3ef69f476e78a7ba8b8ffd
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-08 10:21:40 -0600

    Comment updates

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -2,45 +2,49 @@
 
 import { useState } from "react";
 
+//***''allLocations'' props extraction.
 const CitySearch = ({ allLocations }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
+    //***New state for the input field so that we can access its value (here being query).
     const [query, setQuery] = useState("");
+    //***Local state ''suggestions'' holds the list of suggestions.
     const [suggestions, setSuggestions] = useState([]);
 
-    
-const handleInputChanged = (event) => {
-    const value = event.target.value;
-    const filteredLocations = allLocations ? allLocations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    }) : [];
+    //***Function handleInputChanged is uses as the callback function of  in city search input field below, which is why it has the event parameter in it.
+    //***This function 1) obtains the current value of the input field. Based on this value, it 2) filters the allLocations array, then 3) set the Query local state to whatever the input value is, and finally 4) set the suggestions local state to the filtered locations array.
+    const handleInputChanged = (event) => {
+        const value = event.target.value;
+        const filteredLocations = allLocations ? allLocations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        }) : [];
 
-    setQuery(value);
-    setSuggestions(filteredLocations);
-  };
+        setQuery(value);
+        setSuggestions(filteredLocations);
+    };
 
-  return (
-    <div id="city-search">
-      <input
-        type="text"
-        className="city"
+    return (
+        <div id="city-search">
+            <input
+                type="text"
+                className="city"
                 placeholder="Search for a city"
-        value={query}
-        onFocus={() => setShowSuggestions(true)}
-        onChange={handleInputChanged}
-      />
-      {showSuggestions ?
-        <ul className="suggestions">
-          {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>
-          })}
-          <li key='See all cities'>
-            <b>See all cities</b>
-          </li>
-        </ul>
-        : null
-      }
-    </div>
-  )
+                value={query}
+                onFocus={() => setShowSuggestions(true)}
+                onChange={handleInputChanged}
+            />
+            {showSuggestions ?
+                <ul className="suggestions">
+                    {suggestions.map((suggestion) => {
+                        return <li key={suggestion}>{suggestion}</li>
+                    })}
+                    <li key='See all cities'>
+                        <b>See all cities</b>
+                    </li>
+                </ul>
+                : null
+            }
+        </div>
+    )
 }
 
 export default CitySearch;
