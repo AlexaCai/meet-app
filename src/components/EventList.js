7f8f09d470b8588eda8a9f3a4ba60d19c2b7c33e
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-08 14:22:52 -0600

    Comment update

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,6 +2,7 @@
 
 import Event from "./Event";
 
+//***Props { events } coming from mock-data.js file (mock-data.js is basically an array of JSON objects retrieved from Google Calendar API, with each object representing one event).
 const EventList = ({ events }) => {
  return (
    <ul id="event-list">
