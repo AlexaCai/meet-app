688d6dd157b9e19d350e9a3698ca6c9859d4585b
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-08 14:21:56 -0600

    Implementation of the logic to display event informations

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,43 @@
-// src/components/Event.js
+//***src/components/Event.js
 
-const Event = () => {
-    return (
-      <li></li>
-    );
-  }
-  
-  export default Event;
+//***Import the necessary modules from the 'react' package to allow variable having states.
+import React, { useState } from 'react';
+
+//***Define a functional component called 'Event'.
+//***Props { events } is coming from mock-data.js file (mock-data.js is basically an array of JSON objects retrieved from Google Calendar API, with each object representing one event).
+const Event = ({ event }) => {
+
+  //***Declare a state variable 'showDetails' and a function 'setShowDetails' to update it. Initial value of 'showDetails' is false.
+  const [showDetails, setShowDetails] = useState(false);
+
+  //***Render the component's UI. All event.summary, event.created, etc. comes from the event object structure within mock-data.js.
+  return (
+    <li>
+      {/* Display event summary */}
+      <h4>{event.summary}</h4>
+      {/* Display event start time */}
+      <p>{event.created}</p>
+      {/* Display event location */}
+      <p>{event.location}</p>
+      {/* Button that toggles the 'showDetails' state when clicked. The onClick={() => setShowDetails(!showDetails)} is an event handler that is triggered when the button is clicked. It uses an arrow function to call the setShowDetails function, which updates the showDetails state. The !showDetails expression toggles the current value of showDetails, switching it from false to true or vice versa. */}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {/* showDetails ? 'Hide details' : 'Show details' dynamically changes the text displayed on the event button based on the value of the showDetails state. If showDetails is true, the text will be 'Hide details' (allowing user to go back to collapsed event view). If showDetails is false (so currently in a collapsed event view), the text will be 'Show details' (allowing the user to open more details on an event). */}
+        {showDetails ? 'Hide details' : 'Show details'}
+      </button>
+      {/* Conditionally render the details section based on 'showDetails' state. The contents inside the parentheses following showDetails && are only rendered if the showDetails state is true. */}
+      {showDetails && (
+        //***Show more information about the event after user click on Show details button.
+        <div>
+          {/* Display a heading for the details */}
+          <h4>About the event</h4>
+          {/* Link redirecting to event Google Calendar for more details info */}
+          <p>{event.htmlLink}</p>
+          {/* Display description of the event */}
+          <p>{event.description}</p>
+        </div>
+      )}
+    </li>
+  );
+};
+
+export default Event;
