a8976421d93469ebb0291608ca279f07f1e7d452
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-09 08:58:30 -0600

    Comment updates

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -19,7 +19,6 @@ describe('<Event /> component', () => {
         //***Renders the <Event /> component with the first event object from mockData (mock-data.js) as a prop.
         EventComponent = render(<Event event={mockData[0]} />);
     })
-    
 
     //***Test looking if event's title is rendered.
     test('collapsed event has a title', () => {
@@ -45,25 +44,42 @@ describe('<Event /> component', () => {
         expect(EventComponent.queryByText('Show details')).toBeInTheDocument();
     });
 
+    //***Test looking if all events are collapsed by default.
     test('all events are collapsed by default', () => {
         //***Expecting that the show details button is present in the rendered component.
         expect(EventComponent.queryByText('Show details')).toBeInTheDocument();
     });
 
+    //***Test looking if event details section is shown when a user clicks on Show details button.
     test('event details section is shown when a user clicks on Show details button', async () => {
+        //***Setting up user interactions using the userEvent library.
         const user = userEvent.setup();
+        //***Queries the EventComponent to find an element with the text content 'Show details' (when clicked, reveal more details about an event).
         const showDetailsButton = EventComponent.queryByText('Show details');
+        //***Simulates a user click on the showDetailsButton (use of await indicate that this action involve asynchronous behavior).
         await user.click(showDetailsButton);
+        //***Query the button element that appeared after clicking "Show details" (when a user click on Show detail button, a new view is popping, again with a button ''Hide details'' in it - thats the button targeted here).
         const moreDetails = EventComponent.queryByRole('button');
+        //***Check if the button element above is in the document.
         expect(moreDetails).toBeInTheDocument();
+        //***Check if the text content of the button (after clicking on the button Show details) is "Hide details".
+        expect(moreDetails.textContent).toBe('Hide details');
     });
 
+    //***Test looking if event details section is hidden when a user clicks on Hide details button.
     test('event details section is hidden when a user clicks on Hide details button', async () => {
+        //***Setting up user interactions using the userEvent library.
         const user = userEvent.setup();
-        const showDetailsButton = EventComponent.queryByText('Hide details');
-        await user.click(showDetailsButton);
-        const moreDetails = EventComponent.queryByRole('button');
-        expect(moreDetails).toBeInTheDocument();
+        //***Queries the EventComponent to find an element with the text content 'Hide details' (the button that, when clicked, hide the additional details of an event).
+        const hideDetailsButton = EventComponent.queryByText('Hide details');
+        //***Simulates a user click on the Hide details button.
+        await user.click(hideDetailsButton);
+        //***Query the button element that appeared after clicking "Hide details" (when a user click on Hide detail button, a new view is popping, again with a button ''Show details'' in it - thats the button targeted here).
+        const hideDetails = EventComponent.queryByRole('button');
+        //***Check if the button element above is in the document.
+        expect(hideDetails).toBeInTheDocument();
+        //***Check if the text content of the button (after clicking on the button Hide details) is "Show details".
+        expect(hideDetails.textContent).toBe('Show details');
     });
 
 });
