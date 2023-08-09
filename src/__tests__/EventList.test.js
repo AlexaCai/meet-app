383d7a881e57ffc52ce72a932d943a88d1d9e6e5
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-09 09:12:10 -0600

    Comment updates

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,5 +1,6 @@
-// src/__tests__/EventList.test.js
+//***src/__tests__/EventList.test.js
 
+//***Import necessary modules and files.
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 import { getEvents } from '../api';
@@ -7,16 +8,22 @@ import { getEvents } from '../api';
 describe('<EventList /> component', () => {
     let EventListComponent;
     beforeEach(() => {
-      EventListComponent = render(<EventList />);
+        EventListComponent = render(<EventList />);
     })
-   
+
+    //***This test is checking if the rendered EventListComponent contains an element with a "list" role.
     test('has an element with "list" role', () => {
-      expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
     });
-   
+
+    //***This test is checking whether the EventListComponent renders the correct number of events based on the data fetched from an API.
     test('renders correct number of events', async () => {
-        const allEvents = await getEvents(); 
+        //***fetches all the events using the getEvents() function from API. The use of await indicates that this is an asynchronous operation.
+        const allEvents = await getEvents();
+        //***Re-renders the EventListComponent with the events prop allEvents. This is done to test how the component renders with a specific set of events.
         EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
-      });
-   });
+        //***Queries the EventListComponent to find all elements with the role attribute set to "listitem" using the getAllByRole() function and then checks if the number of found elements matches the length of the allEvents array.
+        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+    });
+
+});
