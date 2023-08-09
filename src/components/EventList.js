65586e3c8af2753b48d733223aaad95c15577a6b
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-09 09:32:00 -0600

    Comment updates

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,4 +1,4 @@
-// src/components/EventList.js
+//***src/components/EventList.js
 
 //***Import necessary module.
 import Event from "./Event";
@@ -8,7 +8,7 @@ const EventList = ({ events }) => {
  return (
   //***Creates an unordered list element with the id attribute set to "event-list". This is the container for the list of events.
    <ul id="event-list">
-     {/* conditional rendering. It checks if the events prop is truthy (not null or undefined). If events is truthy, it renders the content inside the first set of curly braces; otherwise, it renders null. */}
+     {/* Conditional rendering checking if the events prop is truth (not null or undefined). If events is truth, it renders the content inside the first set of curly braces - otherwise, it renders null. */}
      {events ?
        events.map(event => <Event key={event.id} event={event} />) :
        null}
