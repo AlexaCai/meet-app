d927174f68040615047a730a2c8d2f942591b6a8
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-08 18:22:19 -0600

    Test to make sure NumberOfEvent components is well rendered

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -16,4 +16,9 @@ describe('<App /> component', () => {
     test('render CitySearch', () => {
       expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
     });
-  });
+
+    test('render NumberOfEvents', () => {
+        expect(AppDOM.querySelector('#number-events')).toBeInTheDocument();
+      });
+
+  }); 
