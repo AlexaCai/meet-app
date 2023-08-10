7b39a3eaf0feb3b7983f63280040a0c74f0e68ab
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-09 23:18:22 -0600

    Addition of two new integration tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,8 @@
 //***src/__tests__/App.test.js
 
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
 import App from '../App';
 
 //***''describe'' is a function provided by Jest. The test suite is named "<App /> component". The following test suite (containning 3 different tests) is a way to group related tests together for a specific component or functionality.
@@ -28,3 +30,42 @@ describe('<App /> component', () => {
       });
 
   });
+
+  describe('<App /> integration', () => {
+
+    //***This test ensures that the number of rendered events in the UI equals the number of events located in “Berlin, Germany”.
+    test('renders a list of events matching the city selected by the user', async () => {
+
+        //***userEvent is set, and the App component and its DOM are mocked.
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+    
+        //***A reference to the CitySearch component root DOM node is initialized, then a query is performed to find the city input text box in it.
+        const CitySearchDOM = AppDOM.querySelector('#city-search');
+        const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+   
+        //***Simulates typing “Berlin” in the city textbox and then clicking on the list item that contains 'Berlin, Germany'.
+        await user.type(CitySearchInput, "Berlin");
+        const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+        await user.click(berlinSuggestionItem);
+    
+        //***This code queries #event-list (the EventList component root node DOM), and finds what Event list item is rendered inside it. Essential to do this query after clicking on a suggestion list because we expect that the EventList will be affected after selecting that suggestion list item.
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');   
+    
+        //***Gets a list of all events from the mock data that are located in 'Berlin, Germany'
+        const allEvents = await getEvents();
+        const berlinEvents = allEvents.filter(
+          event => event.location === 'Berlin, Germany'
+        );
+    
+        //***Comparing the number of events located in "Berlin, Germany" with the array of rendered Event list items, expecting them to have the same length.
+        expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+
+        //***Make sure that all of the items contain the text 'Berlin, Germany'.
+        allRenderedEventItems.forEach(event => {
+            expect(event.textContent).toContain("Berlin, Germany");
+          });
+      });
+});
