e0886d5289f8802a2deb23a6056ba15cf5687539
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-10 18:36:21 -0600

    Move button at the end of the code (better placement in UI)
    Modification of event.htmlLink to transform it in an hyperlink (cleaner look for user).

--- src/components/Event.js
+++ src/components/Event.js
@@ -14,16 +14,11 @@ const Event = ({ event }) => {
   return (
     <li>
       {/* Display event summary */}
-      <h4>{event.summary}</h4>
+      <h4 id="event-summary">{event.summary}</h4>
       {/* Display event start time */}
       <p>{event.created}</p>
       {/* Display event location */}
       <p>{event.location}</p>
-      {/* Button that toggles the 'showDetails' state when clicked. The onClick={() => setShowDetails(!showDetails)} is an event handler that is triggered when the button is clicked. It uses an arrow function to call the setShowDetails function, which updates the showDetails state. The !showDetails expression toggles the current value of showDetails, switching it from false to true or vice versa. */}
-      <button onClick={() => setShowDetails(!showDetails)}>
-        {/* showDetails ? 'Hide details' : 'Show details' dynamically changes the text displayed on the event button based on the value of the showDetails state. If showDetails is true, the text will be 'Hide details' (allowing user to go back to collapsed event view). If showDetails is false (so currently in a collapsed event view), the text will be 'Show details' (allowing the user to open more details on an event). */}
-        {showDetails ? 'Hide details' : 'Show details'}
-      </button>
       {/* Conditionally render the details section based on 'showDetails' state. The contents inside the parentheses following showDetails && are only rendered if the showDetails state is true. */}
       {showDetails && (
         //***Show more information about the event after user click on Show details button.
@@ -31,11 +26,17 @@ const Event = ({ event }) => {
           {/* Display a heading for the details */}
           <h4>About the event</h4>
           {/* Link redirecting to event Google Calendar for more details info */}
-          <p>{event.htmlLink}</p>
+          <p><a href="{event.htmlLink}" target="_blank">See details on Google Calendar</a></p>
+          <br></br>
           {/* Display description of the event */}
-          <p>{event.description}</p>
+          <p id='event-description'>{event.description}</p>
         </div>
       )}
+      {/* Button that toggles the 'showDetails' state when clicked. The onClick={() => setShowDetails(!showDetails)} is an event handler that is triggered when the button is clicked. It uses an arrow function to call the setShowDetails function, which updates the showDetails state. The !showDetails expression toggles the current value of showDetails, switching it from false to true or vice versa. */}
+      <button id="button-details" onClick={() => setShowDetails(!showDetails)}>
+        {/* showDetails ? 'Hide details' : 'Show details' dynamically changes the text displayed on the event button based on the value of the showDetails state. If showDetails is true, the text will be 'Hide details' (allowing user to go back to collapsed event view). If showDetails is false (so currently in a collapsed event view), the text will be 'Show details' (allowing the user to open more details on an event). */}
+        {showDetails ? 'Hide details' : 'Show details'}
+      </button>
     </li>
   );
 };
