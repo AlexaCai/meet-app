135becd5a0febd7dbc8d32335e36200d6c311705
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-12 11:28:17 -0600

    Code refracting and comment updates

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -11,54 +11,63 @@ import userEvent from '@testing-library/user-event';
 //***loadFeature() function is used to load filterEventsByCity.feature Gherkin file (loadFeature() expects the file path to start from the root of the project).
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
+
 //***Acceptance tests for Feature 1.
+
+
 //***defineFeature() function defines the feature that’s been loaded. 
 defineFeature(feature, test => {
 
-    //***Scenario 1.
-    test('When user hasn’t searched for a specific city, show upcoming events from all cities.', ({ given, when, then }) => {
+
+
+    //***Scenario 1 of feature 1.
+    test('When user hasn’t searched for a specific city, show upcoming events from all cities', ({ given, when, then }) => {
+
         //***In the 'given' step, the precondition is 'user hasn’t searched for any city.' This doesn't require any code as nothing has happened yet (the user hasn’t searched for anything).
         given('user hasn’t searched for any city', () => {
-
         });
 
-        //***The 'when' step is where specifying the main action of the test is needed. In this test, the action is 'the user opens the app'.
         //***AppComponent has been defined outside of the 'when' step because AppComponent will be used more than just the 'when' step (and variables defined within functions aren’t available outside those functions).
+        //***Rendering the App component is equivalent to the app opening and being in initial default view.
         let AppComponent;
         when('user is in the initial default view of the app', () => {
-            //***Rendering the App component is equivalent to the app 'opening and being in initial default view of the app', as it’s the code that would be executed if the user were actually opening up the app.
             AppComponent = render(<App />);
         });
 
         then('user should see a list of upcoming events for all cities.', async () => {
             const AppDOM = AppComponent.container.firstChild;
             const EventListDOM = AppDOM.querySelector('#event-list');
-            //***Getting the list of events is an asynchronous action, which happens in useEffect() of the real component. Use await waitFor() when querying the events list items because we want to give the app time enough to asynchronously fetch events and then update the DOM accordingly. Also why we had to make the callback of the then() step an asynchronous function, by adding the 'async' keyword to it.
+            //***Getting the list of events is an asynchronous action. Use await waitFor() when querying the events list items because we want to give the app time enough to asynchronously fetch events and then update the DOM accordingly. 
             await waitFor(() => {
+                //***Select all list items rendered in UI.
                 const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+                //***Except all list items to of 32.
                 expect(EventListItems.length).toBe(32);
             });
         });
     });
 
-    //***Scenario 2.
-    test('User should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
 
-        //***Simulate the app being opened.
+
+    //***Scenario 2 of feature 1.
+    test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+
+        //***User first open the app, but do nothing else.
+        //***AppComponent has been defined outside of the 'given' step because AppComponent will be used more than just the 'given' step (and variables defined within functions aren’t available outside those functions).
+        //***Rendering the App component is equivalent to the app opening and being in initial default view.
         let AppComponent;
         given('user is in the initial default view of the app', () => {
             AppComponent = render(<App />);
         });
 
-        //***Simulate the user clicking on the city text box and typing "Berlin" into it in the when step. Callback function asynchronous by adding the 'async' keyword to the function (to simulate the asynchronous user actions).
         //***CitySearchDOM is outside the 'when' step because it'll be needed it in the next step (then).
         let CitySearchDOM;
         when('user starts typing in the city search input field', async () => {
             const user = userEvent.setup();
             const AppDOM = AppComponent.container.firstChild;
             CitySearchDOM = AppDOM.querySelector('#city-search');
+            //***Simulate the user clicking on the city search input field and typing "Berlin" into it.
             const citySearchInput = within(CitySearchDOM).queryByRole('textbox');
-            //***type() function is used to simulate the change event on the city text box element, giving it a value of 'Berlin'.
             await user.type(citySearchInput, "Berlin");
         });
 
@@ -70,39 +79,42 @@ defineFeature(feature, test => {
         });
     });
 
-    //***Scenario 3.
-    test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
+
+
+    //***Scenario 3 of feature 1.
+    test('User can select a city from the suggested list', ({ given, and, when, then }) => {
 
         //***All four variables are declared/defined outside of the given() function, because we'll need to use them in later steps in this test, and therefore by being declared outside, we won't have to keep redeclaring them when we want to use them.
         let AppComponent;
         let AppDOM;
         let CitySearchDOM;
         let citySearchInput;
-        //***The user needs to type 'Berlin' into the city textbox and this does require the app to be open. So we still need to render the App component (app being open) and then also need to simulate an event on the city textbox (user typing 'Berlin') in the 'given' test.
+
+        //***User first open the app, but do nothing else.
+        //***Rendering the App component is equivalent to the app opening and being in initial default view.
         //***The 'given' function is async to allow the test to properly simulate any user actions.
-        given('user was typing a specific city (ex: Berlin) in the city search input field', async () => {
+        given('user typed a specific city (ex: Berlin) in the city search input field', async () => {
             //***App component is rendered using the render() function.
             AppComponent = render(<App />);
             const user = userEvent.setup();
             AppDOM = AppComponent.container.firstChild;
             CitySearchDOM = AppDOM.querySelector('#city-search');
+            //***Simulate the user clicking on the city search input field and typing 'Berlin' into it.
             citySearchInput = within(CitySearchDOM).queryByRole('textbox');
-            //***The type() function is used to simulate a change event on the city textbox element, changing its value to 'Berlin'.
             await user.type(citySearchInput, "Berlin");
         });
 
-        //***This step 'and' simply acts as a concatenator, requiring two different preconditions for the test to be executed. The description states that 'the list of suggested cities is showing', so its necessary to implement code that checks whether a list of suggested cities is showing in the body of 'and'.
         //***suggestionListItems is defined outside and() to be able use it in the next when() step.
         let suggestionListItems;
         and('the list of suggested cities is showing', () => {
-            //***Check whether two suggestions are being displayed in App’s list of suggestions (toHaveLength(2)).
+            //***Check whether two suggestions are being displayed in App’s list of suggestions after user typed 'Berlin' (toHaveLength(2)).
             suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
             expect(suggestionListItems).toHaveLength(2);
         });
 
         //***In this 'when' step, we define an action that represents the user selecting a city from the list of suggestions.
         when('user selects a city (ex: Berlin, Germany) from the suggestion list', async () => {
-            //***Simulating a 'click' event on the first suggestion, which in this case, is 'Berlin, Germany' (the second one would be 'See all cities', which is defined in CitySearch component). The 'async' keyword has been added before the callback function passed to when() because it is necessary to simulate user actions inside it.
+            //***Simulating user clicking on the first suggestion, which in this case, is 'Berlin, Germany' (the second one would be 'See all cities', which is defined in CitySearch component). The 'async' keyword has been added before the callback function passed to when() because it is necessary to simulate user actions inside it.
             const user = userEvent.setup();
             await user.click(suggestionListItems[0]);
         });
@@ -117,8 +129,8 @@ defineFeature(feature, test => {
             const EventListDOM = AppDOM.querySelector('#event-list');
             const EventListItems = within(EventListDOM).queryAllByRole('listitem');
             const allEvents = await getEvents();
-            //***filtering the list of all events down to events located in Germany
-            //***citySearchInput.value should have the value "Berlin, Germany" at this point
+            //***Filtering the list of all events down to events located in 'Berlin'.
+            //***CitySearchInput.value should have the value 'Berlin, Germany' at this point
             const berlinEvents = allEvents.filter(event => event.location === citySearchInput.value)
             //***The expect() function checks whether the number of events rendered in the App component equals the length of the array of events located in 'Berlin, Germany' in src/mock-data.js file when the tests are runned.
             expect(EventListItems).toHaveLength(berlinEvents.length);
