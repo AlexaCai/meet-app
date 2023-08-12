51fe5eb2506b6100097239243dbba4aa39188f4b
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-08-12 10:18:12 -0600

    Complete all end-to-end testings for app 3 first features

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,29 +1,31 @@
 import puppeteer from 'puppeteer';
 
-//***End-to-end test for Feature 1.
-describe('Filter Events By City', () => {
 
-    //***Since both of tests require the browser to be opened and closed, we can separate these actions by way of a set of beforeAll() and afterAll() functions (we can code this here before instead of coding these patterns inside each test individually - both methods work, but using beforeAll and afterAll save you time and makes code cleaner, as each test will only include code relevant to that test rather than containing redundant code that applies to every test).
+
+//***End-to-end test for Feature 1.
+
+
+
+describe('Filter events by city', () => {
+
     let browser;
     let page;
     beforeAll(async () => {
-        //***The test starts by launching the browser using Puppeteer (this require the Puppeteer API, so a line of code at the top of the file is needed to import Puppeteer).
+        //***Each test starts by launching the browser using Puppeteer (this require the Puppeteer API, so a line of code at the top of the file is needed to import Puppeteer).
         browser = await puppeteer.launch(
             //     {
             //     //***headless: false make the test turn off headless mode to actually watch the tests being conducted within the browser.
             //     headless: false,
             //     //***Slow down the test in browser by 250ms.
-            //     slowMo: 250, 
+            //     slowMo: 450, 
             //     //***Removes any puppeteer/browser timeout limitations.
             //     timeout: 0 
             //   }
         );
-        //***Puppeteer API is then used to create a new page and navigate to your locally hosted app.
+        //***Puppeteer API is then used to create a new page and navigate to locally hosted app.
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
-        //***localhost:3000 page need some time to load the event list, so to ensure this list is loaded before moving on, we use the API method waitForSelector() to wait until a desired element appears (await page.waitForSelector('.event');). In this case, we want to wait for the 'Event' component to be loaded.
-        //***This test’s purpose is to test the show/hide functionality of the event details, so it’s imperative that the Event component be loaded before; otherwise, there would be nothing to test.
-        await page.waitForSelector('#number-events');
+        await page.waitForSelector('#city-search');
     });
 
     afterAll(() => {
@@ -32,14 +34,17 @@ describe('Filter Events By City', () => {
     });
 
 
-    //***Actual tests.
+    //***Actual tests for Feature 1 - 3 scenarios.
+
 
     //***Test for scenario 1 of feature 1.
     test('When user hasn’t searched for a city, show upcoming events from all cities', async () => {
-        //***Using page.$() for selecting an element on the page. In this case, we select the input[placeholder="Search for a city"].
-        const placeholderElement = await page.$('input[placeholder="Search for a city"]');
-        //***expect() function used to verify whether this extra element (event details) exists or not.
-        expect(placeholderElement).toBeTruthy();
+        //***Using page.$$ to select ALL elements rendered the page that have the class name .event. The $$ function returns an array of elements that match the given selector (.event).
+        //***Using const page.$('.event') would only select the first element with the class name .event on the page. That cant therefore be used to count the only 32 event elements supposed to be shown be default.
+        //***This checks that by default, when user hasn't search for a city, the 32 default events from all cities are initialy shown.
+        const eventElements = await page.$$('.event');
+        //***Using the Jest expect function to make an assertion. eventElements.length gets the number of elements in the eventElements array, which represents the count of event items on the page. toBe(32) is checking if the length of the eventElements array is equal to 32. 
+        expect(eventElements.length).toBe(32);
     });
 
     //***Test for scenario 2 of feature 1.
@@ -47,43 +52,52 @@ describe('Filter Events By City', () => {
         await page.click('.city')
         //***Using page.$() for selecting an element on the page. In this case, we select the .suggestions element.
         const showSuggestions = await page.$('.suggestions');
-        //***expect() function used to verify whether this extra element (event details) exists or not.
+        //***expect() function used to verify whether suggestions list appear or not after clicking on city search input field.
         expect(showSuggestions).toBeTruthy();
     });
 
     //***Test for scenario 3 of feature 1.
     test('User can select a city from the suggested list', async () => {
-        await page.click('.city')
-        //***Using page.$() for selecting an element on the page. In this case, we select the .suggestions element.
-        await page.$('.suggestions');
-        await page.click('li')
+        //***Click on the city input seach field to show suggestions.
+        await page.click('.city');
+        //***Wait for city suggestions to appear.
+        await page.waitForSelector('.suggestions');
+        //***Click on the first city suggestion.
+        await page.click('li');
+        //***Wait for the UI to update.
+        await page.waitForTimeout(500);
+        //***Select an event element in the UI after user have selected a city suggestion.
+        const clickedSuggestion = await page.$('.event');
+        //***Checks if the clickedSuggestion variable (representing the selected city suggestion) is truthy, meaning that the selected city by the user is indeed present in the UI.
+        expect(clickedSuggestion).toBeTruthy();
     });
 });
 
 
-//***End-to-end test for Feature 2.
-describe('show/hide an event details', () => {
 
-    //***Since both of tests require the browser to be opened and closed, we can separate these actions by way of a set of beforeAll() and afterAll() functions (we can code this here before instead of coding these patterns inside each test individually - both methods work, but using beforeAll and afterAll save you time and makes code cleaner, as each test will only include code relevant to that test rather than containing redundant code that applies to every test).
+//***End-to-end test for Feature 2.
+
+
+
+describe('Show/hide an event details', () => {
+
     let browser;
     let page;
     beforeAll(async () => {
-        //***The test starts by launching the browser using Puppeteer (this require the Puppeteer API, so a line of code at the top of the file is needed to import Puppeteer).
+        //***Each test starts by launching the browser using Puppeteer (this require the Puppeteer API, so a line of code at the top of the file is needed to import Puppeteer).
         browser = await puppeteer.launch(
-            //     {
+            // {
             //     //***headless: false make the test turn off headless mode to actually watch the tests being conducted within the browser.
             //     headless: false,
             //     //***Slow down the test in browser by 250ms.
-            //     slowMo: 250, 
+            //     slowMo: 450,
             //     //***Removes any puppeteer/browser timeout limitations.
-            //     timeout: 0 
-            //   }
+            //     timeout: 0
+            // }
         );
         //***Puppeteer API is then used to create a new page and navigate to your locally hosted app.
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
-        //***localhost:3000 page need some time to load the event list, so to ensure this list is loaded before moving on, we use the API method waitForSelector() to wait until a desired element appears (await page.waitForSelector('.event');). In this case, we want to wait for the 'Event' component to be loaded.
-        //***This test’s purpose is to test the show/hide functionality of the event details, so it’s imperative that the Event component be loaded before; otherwise, there would be nothing to test.
         await page.waitForSelector('.event');
     });
 
@@ -93,43 +107,98 @@ describe('show/hide an event details', () => {
     });
 
 
-    //***Actual tests.
+    //***Actual tests for Feature 2 - 3 scenarios.
 
 
     //***Test for scenario 1 of feature 2.
     test('An event element is collapsed by default', async () => {
-        //***Puppeteer checks if 'event details' (const eventDetails = await page.$('.event .details);) is not shown to the user, which is done when the code line 'expect(eventDetails).toBeNull();' is executed.
         //***Using page.$() for selecting an element on the page. In this case, we select the .event .details element, as this is what contains the detailed information of an event.
         const eventDetails = await page.$('.event .details');
-        //***expect() function used to verify whether this extra element (event details) exists or not.
-        expect(eventDetails).toBeNull();
+        //***expect() function used to verify whether event details exists or not (if not exist, it means its collapsed as it should).
+        expect(eventDetails).toBeNull(); 
     });
 
     //***Test for scenario 2 of feature 2.
     test('User can expand an event to see its details', async () => {
-        //***Line which is used to simulate the user clicking on the 'Details' button.
+        //***Simulate the user clicking on the 'Show details' button.
         await page.click('.event .button-details');
-        //***Puppeteer checks if 'event details' (const eventDetails = await page.$('.event .details);) is shown to the user, which is done when the code line 'expect(eventDetails).toBeDefined();' is executed (because user would have click on the 'Show details' button).
         //***Using page.$() for selecting an element on the page. In this case, we select the .event .details element, as this is what contains the detailed information of an event.
         const eventDetails = await page.$('.event .details');
-        //***The expect() function uses the toBeDefined() matcher (instead of the toBeNull() matcher) because in this test we want the event details to exist this time.
+        //***expect() function used to verify whether event details exists or not (if exist, it means its not collapsed as it should after user has clicks on 'Show details' button).
         expect(eventDetails).toBeDefined();
     });
 
     //***Test for scenario 3 of feature 2.
     test('User can collapse an event to hide details', async () => {
+        //***Simulate the user clicking on the 'Show details' button at first.
         await page.click('.event .button-details');
-        //***Puppeteer checks if 'event details' (const eventDetails = await page.$('.event .details);) is shown to the user, which is done when the code line 'expect(eventDetails).toBeDefined();' is executed (because user would have click on the 'Show details' button).
+        //***Simulate the user clicking on the 'Hide details' button after.
+        await page.click('.event .button-details');
         //***Using page.$() for selecting an element on the page. In this case, we select the .event .details element, as this is what contains the detailed information of an event.
         const eventDetails = await page.$('.event .details');
-        //***toBeNull() matcher is used to ensure the extra details element no longer exists when user click on 'Hide details' button.
+        //***expect() function used to verify whether event details exists or not (if not exist, it means its collapsed as it should).
         expect(eventDetails).toBeNull();
     });
 
 
-    // //***End-to-end test for Feature 3.
-    // describe('Specify Number of Events', () => {
 
-    // });
+    //***End-to-end test for Feature 3.
+
+
+
+    describe('Specify number of events', () => {
+
+        // ***Since both of tests require the browser to be opened and closed, we can separate these actions by way of a set of beforeAll() and afterAll() functions (we can code this here before instead of coding these patterns inside each test individually - both methods work, but using beforeAll and afterAll save you time and makes code cleaner, as each test will only include code relevant to that test rather than containing redundant code that applies to every test).
+        let browser;
+        let page;
+        beforeAll(async () => {
+            // ***The test starts by launching the browser using Puppeteer (this require the Puppeteer API, so a line of code at the top of the file is needed to import Puppeteer).
+            browser = await puppeteer.launch(
+                // {
+                //     //***headless: false make the test turn off headless mode to actually watch the tests being conducted within the browser.
+                //     headless: false,
+                //     //***Slow down the test in browser by 250ms.
+                //     slowMo: 450,
+                //     //***Removes any puppeteer/browser timeout limitations.
+                //     timeout: 0
+                // }
+            );
+            // ***Puppeteer API is then used to create a new page and navigate to your locally hosted app.
+            page = await browser.newPage();
+            await page.goto('http://localhost:3000/');
+            await page.waitForSelector('.event');
+        });
+
+        afterAll(() => {
+            // ***Chromium window is closed, completing the test run.
+            browser.close();
+        });
+
+
+        // ***Actual tests for Feature 3 - 2 scenarios.
+
+
+        //***Test for scenario 1 of feature 3.
+        test('When user hasn’t specified a number, 32 events are shown by default', async () => {
+            //***Using page.$$ to select ALL elements rendered in the page that have the classname .event. The $$ function returns an array of elements that match the given selector (.event).
+            //***Using const page.$('.event') would only select the first element with the classname .event on the page. That cannot therefore be used to count the 32 event elements supposed to be shown be default.
+            const eventElements = await page.$$('.event');
+            //***Using the Jest expect function to make an assertion. eventElements.length gets the number of elements in the eventElements array, which represents the count of event elements on the page. toBe(32) is checking if the length of the eventElements array is equal to 32. 
+            expect(eventElements.length).toBe(32);
+        });
+
+        //***Test for scenario 2 of feature 3.
+        test('User can change the number of events displayed', async () => {
+            //***Simulating user clicking on Number of events input field.
+            await page.click('.numberEvents')
+            //***Simulating user typing 3 in the input field.
+            await page.type('.numberEvents', '3');
+            //***Using page.$$ to select ALL elements rendered in the page after user has typed 3. The $$ function returns an array of elements, instead of only the first one element.
+            const eventElements = await page.$$('.event');
+            //***Using the Jest expect function to make an assertion. eventElements.length gets the number of elements in the eventElements array, which represents the count of event items on the page. toBe(32) is checking if the length of the eventElements array is equal to 32. 
+            expect(eventElements.length).toBe(3);
+        });
+
+    });
 
 }); 
